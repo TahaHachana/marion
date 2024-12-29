fc69a59f40e35206240a916376cc6626cd0d462b
Taha Hachana <tahahachana@gmail.com>
2024-12-29 12:52:13 +0100

    Implement starting and stopping a browser

--- examples/basic_usage.rs
+++ examples/basic_usage.rs
@@ -1,3 +1,18 @@
-fn main() {
-    todo!();
+use marion::browser::Browser;
+use tokio;
+use webdriverbidi::{Capabilities, CapabilityRequest};
+
+#[tokio::main]
+async fn main() {
+    let always_match = CapabilityRequest::new();
+    let capabilities = Capabilities::new(always_match);
+
+    let mut browser = Browser::new(capabilities, "localhost", 4444);
+
+    browser.start().await.unwrap();
+
+    // sleep for 5 seconds
+    tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
+
+    browser.stop().await.unwrap();
 }
--- src/browser.rs
+++ src/browser.rs
@@ -1,18 +1,33 @@
+use webdriverbidi::remote::browsing_context::GetTreeParameters;
+use webdriverbidi::session::WebDriverBiDiSession;
+use webdriverbidi::Capabilities;
+
 pub struct Browser {
-    // Fields for browser configuration and state
+    webdriverbidi_session: WebDriverBiDiSession,
+    browsing_context: Option<String>,
 }
 
 impl Browser {
-    pub fn new() -> Self {
-        // Initialize a new browser instance
-        todo!()
+    pub fn new(capabilities: Capabilities, host: &str, port: u16) -> Self {
+        Self {
+            webdriverbidi_session: WebDriverBiDiSession::new(host.to_string(), port, capabilities),
+            browsing_context: None,
+        }
     }
 
-    pub fn start(&self) {
-        // Start the browser
+    pub async fn start(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let _ = self.webdriverbidi_session.start().await?;
+        let get_tree_params = GetTreeParameters::new(None, None);
+        let get_tree_rslt = self
+            .webdriverbidi_session
+            .browsing_context_get_tree(get_tree_params)
+            .await?;
+        self.browsing_context = Some(get_tree_rslt.contexts[0].context.clone());
+        Ok(())
     }
 
-    pub fn stop(&self) {
-        // Stop the browser
+    pub async fn stop(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        self.webdriverbidi_session.close().await?;
+        Ok(())
     }
 }
